fc9d5c9d461cb3b62f7c984e9ec51d9a5951619e
johnsonr <johnsonroda@gmail.com>
2017-12-01 12:45:23 -0800

    Update DenverController.java

--- src/main/java/com/atomist/denver/DenverController.java
+++ src/main/java/com/atomist/denver/DenverController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 @RestController
 class DenverController {
     
-    @Autowired public DemoController() {
+    @Autowired public DenverController() {
     }
 
     /**
