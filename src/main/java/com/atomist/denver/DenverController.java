e5eeee04a0c25f4aee17f99569e1a59db5a917a5
johnsonr <johnsonroda@gmail.com>
2017-12-01 12:44:12 -0800

    Update DenverController.java

--- src/main/java/com/atomist/denver/DenverController.java
+++ src/main/java/com/atomist/denver/DenverController.java
@@ -3,9 +3,13 @@ package com.atomist.denver;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 class DenverController {
+    
+    @Autowired public DemoController() {
+    }
 
     /**
      * Access via http://localhost:8081/person/name
